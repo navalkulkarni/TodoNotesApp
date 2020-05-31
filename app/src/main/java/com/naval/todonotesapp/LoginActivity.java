e29b15604a9e957ab2dc268d8c43953796897e79
Naval Kulkarni <navalkulkarni@gmail.com>
2020-05-31 16:28:27 +0530

    Passed data from LoginActivity to MyNotesActivity using action listener and intent.

--- app/src/main/java/com/naval/todonotesapp/LoginActivity.java
+++ app/src/main/java/com/naval/todonotesapp/LoginActivity.java
@@ -2,13 +2,38 @@ package com.naval.todonotesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText editTextFullName,editTextUserName;
+    Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+
+        editTextFullName = findViewById(R.id.editTextFullName);
+        editTextUserName = findViewById(R.id.editTextUserName);
+        buttonLogin = findViewById(R.id.materialButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Click","On Click Performed");
+                String fullName = editTextFullName.getText().toString();
+                Intent intent = new Intent(LoginActivity.this,MyNotesActivity.class);
+                intent.putExtra("full_name",fullName);
+                startActivity(intent);
+            }
+        };
+
+        buttonLogin.setOnClickListener(listener);
     }
 }
--- app/src/main/java/com/naval/todonotesapp/MyNotesActivity.java
+++ app/src/main/java/com/naval/todonotesapp/MyNotesActivity.java
@@ -2,7 +2,9 @@ package com.naval.todonotesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MyNotesActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MyNotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_notes);
+
+        Intent intent = getIntent();
+        Log.d("IntentDataPass",intent.getStringExtra("full_name"));
     }
 }
