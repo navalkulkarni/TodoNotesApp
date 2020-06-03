7e2ae6c8fd185c3fafe6b8c00f690c8d842fd8dc
Naval Kulkarni <navalkulkarni@gmail.com>
2020-06-03 10:56:56 +0530

    added plugins and classpath to build.gradle project file and app file for KOTLIN conversion. Also, converted MainActivity.kt and SplashActivity.kt to Kotlin using IDE.

---
+++ app/src/main/java/com/naval/todonotesapp/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.naval.todonotesapp
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    var sharedPreferences: SharedPreferences? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupSharedPreference()
+        checkLoginStatus()
+    }
+
+    private fun setupSharedPreference() {
+        sharedPreferences = getSharedPreferences(PrefConstant.SHARED_PREF_NAME, Context.MODE_PRIVATE)
+    }
+
+    private fun checkLoginStatus() {
+        val isLoggedIn = sharedPreferences!!.getBoolean(PrefConstant.LOGGED_IN_STATUS, false)
+        if (isLoggedIn) {
+            val intent = Intent(this@SplashActivity, MyNotesActivity::class.java)
+            startActivity(intent)
+        } else {
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
