6363593fbace2960f24f0839ecc9a49b2c42eda3
Naval Kulkarni <navalkulkarni@gmail.com>
2020-06-04 20:24:57 +0530

    create AddNotesActivity.kt and create a layout for it

---
+++ app/src/main/java/com/naval/todonotesapp/view/AddNotesActivity.kt
@@ -0,0 +1,12 @@
+package com.naval.todonotesapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.naval.todonotesapp.R
+
+class AddNotesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_notes)
+    }
+}
