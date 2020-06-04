01d3fc74599cd95b7a56ccfc8724ed75d092bdf2
Naval Kulkarni <navalkulkarni@gmail.com>
2020-06-04 12:35:02 +0530

    wrote function for accessing db instance from Application class NotesApp.kt

--- app/src/main/java/com/naval/todonotesapp/NotesApp.kt
+++ app/src/main/java/com/naval/todonotesapp/NotesApp.kt
@@ -1,10 +1,15 @@
 package com.naval.todonotesapp
 
 import android.app.Application
+import com.naval.todonotesapp.db.NotesDatabase
 
 class NotesApp: Application() {
 
     override fun onCreate() {
         super.onCreate()
+    }
+
+    fun getNotesDb():NotesDatabase{
+        return NotesDatabase.getInstance(this)
     }
 }
