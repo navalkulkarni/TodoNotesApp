f6650f497e0fd3a95d7d4acd7b1e1634ad410bbb
Naval Kulkarni <navalkulkarni@gmail.com>
2020-06-06 16:51:40 +0530

    add notification channel and create first notification. Also, add method for getting token from firebase into SplashActivity.kt

--- app/src/main/java/com/naval/todonotesapp/fcm/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/naval/todonotesapp/fcm/MyFirebaseMessagingService.kt
@@ -1,6 +1,48 @@
 package com.naval.todonotesapp.fcm
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.media.Ringtone
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.JsonToken
+import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.naval.todonotesapp.R
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
+    val TAG = "Fireball"
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        Log.d(TAG,message?.from)
+        Log.d(TAG,message?.notification?.body)
+        setupNotification(message?.notification?.body)
+    }
+
+    private fun setupNotification(body: String?) {
+        val channelID = "ToDoNotes"
+        val ringtone = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notification = NotificationCompat.Builder(this,channelID)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentText(body)
+                .setSound(ringtone)
+                .setContentTitle("Todo Notes App")
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if(Build.VERSION.SDK_INT>Build.VERSION_CODES.O)
+        {
+            val channel = NotificationChannel(channelID,"Todo Notes",NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(0,notification.build())
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+
 }
--- app/src/main/java/com/naval/todonotesapp/view/SplashActivity.kt
+++ app/src/main/java/com/naval/todonotesapp/view/SplashActivity.kt
@@ -4,17 +4,42 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
 import com.naval.todonotesapp.R
 import com.naval.todonotesapp.utils.PrefConstant
 
 class SplashActivity : AppCompatActivity() {
     lateinit var sharedPreferences: SharedPreferences
+    val TAG = "FirebaseToken"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         setupSharedPreference()
         checkLoginStatus()
+        getFirebaseToken()
+    }
+
+    private fun getFirebaseToken() {
+        FirebaseInstanceId.getInstance().instanceId
+                .addOnCompleteListener(OnCompleteListener { task ->
+                    if (!task.isSuccessful) {
+                        Log.w(TAG, "getInstanceId failed", task.exception)
+                        return@OnCompleteListener
+                    }
+
+                    // Get new Instance ID token
+                    val token = task.result?.token
+
+                    // Log and toast
+
+                    Log.d(TAG, token)
+                    Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+                })
     }
 
     private fun setupSharedPreference() {
