db7b81e177cedd36948cc243415802872b9ec04b
Naval Kulkarni <navalkulkarni@gmail.com>
2020-06-08 09:47:25 +0530

    add two blank fragments for OnboardingActivity.kt

---
+++ app/src/main/java/com/naval/todonotesapp/onboarding/OnboardingActivity.kt
@@ -0,0 +1,13 @@
+
+package com.naval.todonotesapp.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.naval.todonotesapp.R
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
