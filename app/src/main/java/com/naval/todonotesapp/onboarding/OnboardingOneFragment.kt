0f9f17f1022dd109feb0878f8db0458f3feee16e
Naval Kulkarni <navalkulkarni@gmail.com>
2020-06-08 10:41:51 +0530

    bind views of widgets in fragments

--- app/src/main/java/com/naval/todonotesapp/onboarding/OnboardingOneFragment.kt
+++ app/src/main/java/com/naval/todonotesapp/onboarding/OnboardingOneFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.naval.todonotesapp.R
 
 
 class OnboardingOneFragment : Fragment() {
 
+    lateinit var nextFragmentOne:TextView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -17,5 +19,12 @@ class OnboardingOneFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_onboarding_one, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindView(view)
+    }
 
+    private fun bindView(view: View) {
+        nextFragmentOne = view.findViewById(R.id.nextFragmentOne)
+    }
 }
--- app/src/main/java/com/naval/todonotesapp/onboarding/OnboardingTwoFragment.kt
+++ app/src/main/java/com/naval/todonotesapp/onboarding/OnboardingTwoFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.naval.todonotesapp.R
 
 
 class OnboardingTwoFragment : Fragment() {
 
+    lateinit var nextFragmentTwo:TextView
+    lateinit var backFragmentTwo:TextView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -17,5 +20,14 @@ class OnboardingTwoFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_onboarding_two, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindView(view)
+    }
+
+    private fun bindView(view: View) {
+        backFragmentTwo = view.findViewById(R.id.backFragmentTwo)
+        nextFragmentTwo = view.findViewById(R.id.nextFragmentTwo)
+    }
 
 }
