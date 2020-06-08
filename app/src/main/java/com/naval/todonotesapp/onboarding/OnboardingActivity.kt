61aed84baec2a31451923625909a03eedc22f718
Naval Kulkarni <navalkulkarni@gmail.com>
2020-06-08 10:32:19 +0530

    design rough layouts for fragments and OnboardingActivity.kt

--- app/src/main/java/com/naval/todonotesapp/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/naval/todonotesapp/onboarding/OnboardingActivity.kt
@@ -3,11 +3,20 @@ package com.naval.todonotesapp.onboarding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
 import com.naval.todonotesapp.R
 
 class OnboardingActivity : AppCompatActivity() {
+
+    lateinit var viewPager:ViewPager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
+        bindView()
+    }
+
+    private fun bindView() {
+        viewPager = findViewById(R.id.viewPagerOnBoarding)
     }
 }
